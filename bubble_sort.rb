971394afaa52899139e5a868831e1a41b2f2d8e7
sipe-daniel <sipetchawoudanieljordan@yahoo.fr>
2020-07-07 23:57:27 +0100

    make the code readable

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -4,56 +4,52 @@ places = gets.chomp.to_i
 array = Array.new(places)
 
 array.each_with_index do |number, i|
-    puts "please, let me know the ##{i+1}"
-    array[i]= gets.chomp
+  puts "please, let me know the ##{i + 1}"
+  array[i] = gets.chomp
 end
+
 $length = array.length
 
 def bubble_sort (an_array)
-
-i = 0
-while i < $length do 
+  i = 0
+  while i < $length do
     j = 1
     counter = 0
-    while j < $length do 
-        if an_array[j-1].to_i > an_array[j].to_i
-            an_array[j-1], an_array[j] = an_array [j], an_array[j-1]
-            counter += 1
-        end 
-        j += 1
+    while j < $length do
+      if an_array[j - 1].to_i > an_array[j].to_i
+          an_array[j - 1], an_array[j] = an_array [j], an_array[j - 1]
+          counter += 1
+      end
+      j += 1
     end
-    counter == 0 ? break : i +=1 
-end 
+    counter == 0 ? break : i +=1
+  end
 an_array
 end
 
 def bubble_sort_by(an_array)
-   
-    i = 0
-   while i < $length do
-     j = 1
-     counter = 0
-     while j < $length do 
-       if yield(an_array[j-1], an_array[j]) > 0 
-           an_array[j-1], an_array[j] = an_array[j], an_array[j-1]
-           counter += 1
-       end 
-       j += 1
-     end
-     counter == 0 ? break : i +=1
-   
-   end
-   an_array     
-   end
+  i = 0
+  while i < $length do
+    j = 1
+    counter = 0
+    while j < $length do
+      if yield(an_array[j - 1], an_array[j]) > 0
+        an_array[j - 1], an_array[j] = an_array[j], an_array[j - 1]
+        counter += 1
+      end
+      j += 1
+    end
+    counter == 0 ? break : i += 1
+  end
+  an_array
+end
 
-   bubble_sort_by(array) do |left, right|
-    left.length - right.length    
-   end
-     
+bubble_sort_by(array) do |left, right|
+  left.length - right.length
+end
+
 puts "Array sorted chain"
-p array 
+p array
 
 puts "Array sorted number"
 p bubble_sort(array)
-
-
